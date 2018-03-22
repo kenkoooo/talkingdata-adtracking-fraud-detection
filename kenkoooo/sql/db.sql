ebbd2809b35bff1bf80092c2b713f49a834771e3
kenkoooo <kenkou.n@gmail.com>
2018-03-23 01:26:48 +0900

    Create db.sql

---
+++ kenkoooo/sql/db.sql
@@ -0,0 +1,32 @@
+CREATE TABLE click_data (
+  id          SERIAL PRIMARY KEY,
+  click_id    BIGINT,
+  ip          BIGINT NOT NULL,
+  app         BIGINT NOT NULL,
+  device      BIGINT NOT NULL,
+  os          BIGINT NOT NULL,
+  channel     BIGINT NOT NULL,
+  click_time  TIMESTAMP,
+  attributed_time TIMESTAMP,
+  is_attributed INT
+);
+
+# test.csv 受け皿用の一時テーブル作成
+CREATE TEMPORARY TABLE T(
+  click_id    BIGINT,
+  ip          BIGINT NOT NULL,
+  app         BIGINT NOT NULL,
+  device      BIGINT NOT NULL,
+  os          BIGINT NOT NULL,
+  channel     BIGINT NOT NULL,
+  click_time  TIMESTAMP
+);
+
+# 一時テーブルに展開
+\copy t from /home/ubuntu/talkingdata-adtracking-fraud-detection/data/test.csv DELIMITER "," CSV HEADER;
+
+# 一時テーブルからコピー
+INSERT INTO click_data (click_id,ip,app,device,os,channel,click_time) SELECT click_id,ip,app,device,os,channel,click_time FROM t
+
+# 一時テーブル削除
+drop table t;
