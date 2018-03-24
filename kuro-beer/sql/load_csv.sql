b269c7ad8f248ac0940dfc02d18e4b1a8c7cff6d
Ubuntu <ubuntu@ip-172-31-15-19.us-east-2.compute.internal>
2018-03-24 15:27:25 +0000

    update

--- kuro-beer/sql/load_csv.sql
+++ kuro-beer/sql/load_csv.sql
@@ -1,6 +1,8 @@
-#############################
-#     DATA SET UP           #
-#############################
+/*----------------------------------------------------------*/
+/* data set-up                                              */
+/*----------------------------------------------------------*/
+
+
 CREATE TABLE click_data (
   id          SERIAL PRIMARY KEY,
   click_id    BIGINT,
@@ -14,8 +16,8 @@ CREATE TABLE click_data (
   is_attributed INT
 );
 
-#--------------------------------------------------------
-# test.csv 受け皿用の一時テーブル作成
+/*----------------------------------------------------------*/
+-- test.csv 受け皿用の一時テーブル作成
 CREATE TEMPORARY TABLE t(
   click_id    BIGINT,
   ip          BIGINT NOT NULL,
@@ -26,18 +28,18 @@ CREATE TEMPORARY TABLE t(
   click_time  TIMESTAMP
 );
 
-# 一時テーブルに展開
+-- 一時テーブルに展開 
 \copy t from '/home/ubuntu/talkingdata-adtracking-fraud-detection/data/test.csv' DELIMITER ',' CSV HEADER;
 
-# 一時テーブルからコピー
+-- 一時テーブルからコピー 
 INSERT INTO click_data (click_id,ip,app,device,os,channel,click_time)
        SELECT click_id,ip,app,device,os,channel,click_time FROM t;
 
-# 一時テーブル削除
+-- 一時テーブル削除 
 drop table t;
 
-#--------------------------------------------------------
-# train.csv 受け皿用の一時テーブル作成
+/*----------------------------------------------------------*/
+-- train.csv 受け皿用の一時テーブル作成
 CREATE TEMPORARY TABLE t(
   ip          BIGINT NOT NULL,
   app         BIGINT NOT NULL,
@@ -49,14 +51,14 @@ CREATE TEMPORARY TABLE t(
   is_attributed INT
 );
 
-# 一時テーブルに展開
-\copy t from '/home/ubuntu/talkingdata-adtracking-fraud-detection/data/tarin.csv' DELIMITER ',' CSV HEADER;
+-- 一時テーブルに展開
+\copy t from '/home/ubuntu/talkingdata-adtracking-fraud-detection/data/train.csv' DELIMITER ',' CSV HEADER;
 
-# 一時テーブルからコピー
+-- 一時テーブルからコピー
 INSERT INTO click_data (ip,app,device,os,channel,click_time,attributed_time,is_attributed)
-       SELECT click_id,ip,app,device,os,channel,click_time,attributed_time,is_attributed FROM t;
+       SELECT ip,app,device,os,channel,click_time,attributed_time,is_attributed FROM t;
 
-# 一時テーブル削除
+-- 一時テーブル削除
 drop table t;
 
 
